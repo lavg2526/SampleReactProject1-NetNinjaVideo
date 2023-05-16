d19d8a5db36ad93765ed4a98eda11b00dac50b14
lavg2526 <55224971+lavg2526@users.noreply.github.com>
2023-05-16 19:34:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h3>Mango Santhe</h3>
+      <Home />
+    </div>
+  );
+}
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1> Its Mango Season </h1>
+      <nav className="links">
+        <a href="/">Types </a>
+        <a className="price" href="/Price">
+          Price
+        </a>
+      </nav>
+    </div>
+  );
+};
+export default Navbar;
