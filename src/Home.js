69af8147185bab0a741a863718b4264d2adf50d8
lavg2526 <55224971+lavg2526@users.noreply.github.com>
2023-05-19 16:08:41 +0530

    CreatedwithNewcomponentMangoList to get the mango types (#1)

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,34 @@
+import { useState } from "react";
+import MangoList from "./MangoList";
 const Home = () => {
-  return <div> HomePage </div>;
+  const [blogs, setBlogs] = useState([
+    {
+      title: "Raspuri Mango",
+      origin: "Mysore",
+      id: 1,
+    },
+    {
+      title: "Imam Pasand Mango",
+      origin: "Tamil Nadu",
+      id: 2,
+    },
+    {
+      title: "Alphonso Mango",
+      origin: "Portuguese",
+      id: 3,
+    },
+  ]);
+  const handleDelete = (id) => {
+    setBlogs(
+      blogs.filter((blog) => {
+        return blog.id !== id;
+      })
+    );
+  };
+  return (
+    <div>
+      <MangoList blogs={blogs} handleDelete={handleDelete} />
+    </div>
+  );
 };
 export default Home;
